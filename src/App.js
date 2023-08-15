925a54fd80365850568eb6dbc13572726deb8fab
kynkcaycan <105385269+kynkcaycan@users.noreply.github.com>
2023-08-15 17:25:36 +0300

    degistirilcek

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Button from '@mui/material/Button';
 
+
+
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
 function App() {
+
   return (
-    <div className="App">
-       <header className="App-header">My Potfolio</header>
-      <div className="butons">
-      <Button variant ="contained" color="secondary" size="large">Buy</Button>
-      <Button variant ="contained" color="secondary" size="large">Sell</Button>
-      <Button variant ="contained" color="secondary" size="large">Portfolio</Button>
+    <Router>
+      <div className="App">
+
+        <div className="middleBox">
+          <header className="App-header">My Potfolio</header>
+      
+            <Switch>
+              <Route path="/a">
+              <Button variant ="contained" color="secondary" size="large">Buy</Button>
+             <Button variant ="contained" color="secondary" size="large">Sell</Button>
+              <Button variant ="contained" color="secondary" size="large">Portfolio</Button> 
+
+              </Route>
+            </Switch>
+
+
+          
+        </div>
+
       </div>
-    </div>
+    </Router>
+
   );
 }
 
