86aa271a6ac99a3c0ae06e421cb391807a4c02f7
kynkcaycan <105385269+kynkcaycan@users.noreply.github.com>
2023-08-15 15:02:59 +0300

    butonlar eklendi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-      </header>
+       <header className="App-header">My Potfolio</header>
+      <div className="butons">
+      <Button variant ="contained" color="secondary" size="large">Buy</Button>
+      <Button variant ="contained" color="secondary" size="large">Sell</Button>
+      <Button variant ="contained" color="secondary" size="large">Portfolio</Button>
+      </div>
     </div>
   );
 }
