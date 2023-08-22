8bf0511eca941fcf31ebf9aceec41820985e1b0a
Fatma Nur Korkmaz <fnur.korkmaz13@gmail.com>
2023-08-22 13:40:49 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
                </Route>
 
               <Route path="/sell">
-                <BuyPage/>
+                <SellPage/>
               </Route>
 
               <Route path="/buying">
