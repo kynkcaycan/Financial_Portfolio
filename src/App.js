232c2a21a4dd198013b088944f2a0269f5d8e373
Fatma Nur Korkmaz <fnur.korkmaz13@gmail.com>
2023-08-25 13:42:21 +0300

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // App.js
-import './App.css'; // App.js içinde CSS dosyasını içe aktarıyorsunuz
+import "./App.css"; // App.js içinde CSS dosyasını içe aktarıyorsunuz
 
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Typography } from '@mui/material';
-import BuyPage from './Pages/BuyPage';
-import api from './api/axiosConfig';
-import ButonsMain from './butonsMain';
-import BuyProcessPage from './Pages/BuyProcessPage';
-import MyPortfolioPage from './Pages/myPortfolioPage';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Typography } from "@mui/material";
+import BuyPage from "./Pages/BuyPage";
+import api from "./api/axiosConfig";
+import ButonsMain from "./butonsMain";
+import BuyProcessPage from "./Pages/BuyProcessPage";
+import MyPortfolioPage from "./Pages/myPortfolioPage";
 
 function App() {
   const [doviz, setDoviz] = useState([]);
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     const getDoviz = async () => {
       try {
-        const response = await api.get("/api/v1/golds");
+        const response = await api.get("/api/v1/doviz");
         setDoviz(response.data);
       } catch (err) {
         console.log(err);
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="App">
-      <Typography variant='h1' >My Portfolio</Typography>
+      <Typography variant="h1">My Portfolio</Typography>
       <Router>
         <div className="middleBox">
           <Switch>
--- src/butonsMain.js
+++ src/butonsMain.js
@@ -31,33 +31,8 @@ const ButonsMain = () => {
           Portfolio
         </Button>{" "}
       </div>
-      <div class="mainButons">
-        {" "}
-        <Button onClick={clickSell} variant="contained" color="success">
-          Sell
-        </Button>
-      </div>
-      <div class="mainButons">
-        <Button variant="contained" color="success">
-          Portfolio
-        </Button>{" "}
-      </div>
     </div>
   );
 };
 
-        window.location.href='/portfolio';
-    }
-        
-    
-    return (
-        <div className='buysellport'>
-            <div class='mainButons' > <Button onClick={clickBuy}
-                variant="contained"  color='success'>Buy</Button></div>
-            <div class='mainButons' > <Button variant="contained" color='success'>Sell</Button></div>
-            <div class='mainButons'><Button onClick={clickPortfolio} variant="contained" color='success' >Portfolio</Button> </div>
-            <div class='mainButons' > <Button onClick={clickSell} 
-                variant="contained" color='success'>Sell</Button></div>
-            <div class='mainButons'><Button onClick={clickPortfolio}
-                variant="contained" color='success' >Portfolio</Button> </div>
-    const clickPortfolio=()=>{
+export default ButonsMain;
